ef1a88de6bed060560d63388bbca57db146744e9
wnsgh <junho767@naver.com>
2025-03-06 17:05:11 +0900

    refactor: 학습 컨텐츠 페이지 너비 조정 및 헤더 수정

--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -47,11 +47,10 @@ export default function Header() {
           ) : (
             <>
               <li>
-                <Link href="/studyContent">CS 전공지식 학습하기</Link>
                 <Link href="/">MAIN</Link>
               </li>
               <li>
-                <Link href="/">CS 전공지식 학습하기</Link>
+                <Link href="/studyContent">CS 전공지식 학습하기</Link>
               </li>
               {/* 드롭다운 메뉴 시작 */}
               <li className={styles.dropdown}>
