bb49531ded4e5c7029282c7874f06ae5ec5c8079
wnsgh <junho767@naver.com>
2025-03-07 16:39:24 +0900

    refactor: 멤버 정보 조회 방법 변경
    1. Rq로 접근하고 있다가 memberService를 통해 접근하게끔 수정
    2. 학습 페이지 카테고리 -> 학습자료 로 수정

--- backend/src/main/java/com/java/NBE4_5_1_7/domain/study/service/StudyMemoService.java
+++ backend/src/main/java/com/java/NBE4_5_1_7/domain/study/service/StudyMemoService.java
@@ -1,11 +1,7 @@
 package com.java.NBE4_5_1_7.domain.study.service;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.springframework.stereotype.Service;
-
 import com.java.NBE4_5_1_7.domain.member.entity.Member;
+import com.java.NBE4_5_1_7.domain.member.service.MemberService;
 import com.java.NBE4_5_1_7.domain.study.dto.request.StudyMemoRequestDto;
 import com.java.NBE4_5_1_7.domain.study.dto.response.StudyMemoResponseDto;
 import com.java.NBE4_5_1_7.domain.study.entity.FirstCategory;
@@ -13,21 +9,23 @@ import com.java.NBE4_5_1_7.domain.study.entity.StudyContent;
 import com.java.NBE4_5_1_7.domain.study.entity.StudyMemo;
 import com.java.NBE4_5_1_7.domain.study.repository.StudyContentRepository;
 import com.java.NBE4_5_1_7.domain.study.repository.StudyMemoRepository;
-import com.java.NBE4_5_1_7.global.Rq;
 import com.java.NBE4_5_1_7.global.exception.ServiceException;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class StudyMemoService {
     private final StudyMemoRepository studyMemoRepository;
     private final StudyContentRepository studyContentRepository;
-    private final Rq rq;
+    private final MemberService memberService;
 
     // 멤버, 학습 컨텐츠 ID, 메모 내용 저장
     public void createStudyMemo(String studyMemoContent, Long studyContentId) {
-        Member member = rq.getActor();
+        Member member = memberService.getMemberFromRq();
 
         StudyContent studyContent = studyContentRepository.findById(studyContentId)
                 .orElseThrow(() -> new RuntimeException("존재하지 않는 학습 컨텐츠 입니다."));
@@ -40,20 +38,20 @@ public class StudyMemoService {
         List<StudyMemo> memos = studyMemoRepository.findByMemberAndStudyContentCategory(member, category);
 
         return memos.stream()
-            .map(memo -> new StudyMemoResponseDto(
-                memo.getId(),
-                memo.getMemoContent(),
-                memo.getStudyContent().getStudy_content_id(),
-                memo.getStudyContent().getFirstCategory().getCategory(),
-                memo.getStudyContent().getTitle(),
-                memo.getStudyContent().getBody()
-            ))
-            .collect(Collectors.toList());
+                .map(memo -> new StudyMemoResponseDto(
+                        memo.getId(),
+                        memo.getMemoContent(),
+                        memo.getStudyContent().getStudy_content_id(),
+                        memo.getStudyContent().getFirstCategory().getCategory(),
+                        memo.getStudyContent().getTitle(),
+                        memo.getStudyContent().getBody()
+                ))
+                .collect(Collectors.toList());
     }
 
     public StudyMemoResponseDto updateStudyMemo(Long studyMemoId, StudyMemoRequestDto updatedDto, Member member) {
         StudyMemo studyMemo = studyMemoRepository.findById(studyMemoId)
-            .orElseThrow(() -> new RuntimeException("해당 메모를 찾을 수 없습니다."));
+                .orElseThrow(() -> new RuntimeException("해당 메모를 찾을 수 없습니다."));
 
         if (!studyMemo.getMember().equals(member)) {
             throw new ServiceException("403", "본인이 작성한 메모만 수정할 수 있습니다.");
@@ -63,18 +61,18 @@ public class StudyMemoService {
         StudyMemo updatedMemo = studyMemoRepository.save(studyMemo);
 
         return new StudyMemoResponseDto(
-            updatedMemo.getId(),
-            updatedMemo.getMemoContent(),
-            updatedMemo.getStudyContent().getStudy_content_id(),
-            updatedMemo.getStudyContent().getFirstCategory().getCategory(),
-            updatedMemo.getStudyContent().getTitle(),
-            updatedMemo.getStudyContent().getBody()
+                updatedMemo.getId(),
+                updatedMemo.getMemoContent(),
+                updatedMemo.getStudyContent().getStudy_content_id(),
+                updatedMemo.getStudyContent().getFirstCategory().getCategory(),
+                updatedMemo.getStudyContent().getTitle(),
+                updatedMemo.getStudyContent().getBody()
         );
     }
 
     public void deleteStudyMemo(Long studyMemoId, Member member) {
         StudyMemo studyMemo = studyMemoRepository.findById(studyMemoId)
-            .orElseThrow(() -> new RuntimeException("해당 메모를 찾을 수 없습니다."));
+                .orElseThrow(() -> new RuntimeException("해당 메모를 찾을 수 없습니다."));
 
         if (!studyMemo.getMember().equals(member)) {
             throw new ServiceException("403", "본인이 작성한 댓글만 삭제할 수 있습니다.");
