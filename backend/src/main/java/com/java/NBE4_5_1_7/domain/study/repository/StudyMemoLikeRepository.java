d3a45f6e28fba1d49dd00e8c7c67e74302b03ad6
junho767 <junho767@naver.com>
2025-03-10 15:15:19 +0900

    refactor: 코드 리펙토링

--- backend/src/main/java/com/java/NBE4_5_1_7/domain/study/repository/StudyMemoLikeRepository.java
+++ backend/src/main/java/com/java/NBE4_5_1_7/domain/study/repository/StudyMemoLikeRepository.java
@@ -2,9 +2,7 @@ package com.java.NBE4_5_1_7.domain.study.repository;
 
 import com.java.NBE4_5_1_7.domain.study.entity.StudyMemoLike;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface StudyMemoLikeRepository extends JpaRepository<StudyMemoLike, Integer> {
-    @Query("SELECT COUNT(s) FROM StudyMemoLike s WHERE s.studyMemo.id = :studyMemoId AND s.studyMemo.isPublished = true")
     int countByStudyMemoId(Long studyMemoId);
 }
--- backend/src/main/java/com/java/NBE4_5_1_7/domain/study/repository/StudyMemoRepository.java
+++ backend/src/main/java/com/java/NBE4_5_1_7/domain/study/repository/StudyMemoRepository.java
@@ -14,7 +14,10 @@ public interface StudyMemoRepository extends JpaRepository<StudyMemo, Long> {
 	@Query("SELECT c FROM StudyMemo c JOIN c.studyContent ic " +
 		"WHERE c.member = :member AND ic.firstCategory = :category")
 	List<StudyMemo> findByMemberAndStudyContentCategory(@Param("member") Member member, @Param("category") FirstCategory category);
+
+	@Query("SELECT s FROM StudyMemo s WHERE s.studyContent = :studyContent AND s.isPublished = true")
 	List<StudyMemo> findByStudyContent(StudyContent studyContent);
+
 
 	StudyMemo findByMemberAndStudyContent(Member member, StudyContent studyContentId);
 }
