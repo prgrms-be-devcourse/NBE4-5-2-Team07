e4fbeaf811cec4e4f3119914fc28708088f23b4f
junho767 <junho767@naver.com>
2025-03-10 10:04:57 +0900

    feat: 학습 메모 좋아요 기능 추가

---
+++ backend/src/main/java/com/java/NBE4_5_1_7/domain/study/repository/StudyMemoLikeRepository.java
@@ -0,0 +1,7 @@
+package com.java.NBE4_5_1_7.domain.study.repository;
+
+import com.java.NBE4_5_1_7.domain.study.entity.StudyMemoLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudyMemoLikeRepository extends JpaRepository<StudyMemoLike, Integer> {
+}
---
+++ backend/src/main/java/com/java/NBE4_5_1_7/domain/study/service/StudyMemoLikeService.java
@@ -0,0 +1,24 @@
+package com.java.NBE4_5_1_7.domain.study.service;
+
+import com.java.NBE4_5_1_7.domain.member.entity.Member;
+import com.java.NBE4_5_1_7.domain.member.service.MemberService;
+import com.java.NBE4_5_1_7.domain.study.entity.StudyMemo;
+import com.java.NBE4_5_1_7.domain.study.entity.StudyMemoLike;
+import com.java.NBE4_5_1_7.domain.study.repository.StudyMemoLikeRepository;
+import com.java.NBE4_5_1_7.domain.study.repository.StudyMemoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StudyMemoLikeService {
+    private final StudyMemoLikeRepository studyMemoLikeRepository;
+    private final StudyMemoRepository studyMemoRepository;
+    private final MemberService memberService;
+
+    public void addLike(Long studyMemoId) {
+        Member member = memberService.getMemberFromRq();
+        StudyMemo studyMemo = studyMemoRepository.findById(studyMemoId).orElse(null);
+        studyMemoLikeRepository.save(new StudyMemoLike(member, studyMemo));
+    }
+}
