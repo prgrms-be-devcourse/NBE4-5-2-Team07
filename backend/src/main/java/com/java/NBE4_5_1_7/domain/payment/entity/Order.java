5ad1cc0a1068ef5ec5fa74694326cd848ae87a2b
junho767 <junho767@naver.com>
2025-03-11 14:43:29 +0900

    feat: Order 테이블 생성

---
+++ backend/src/main/java/com/java/NBE4_5_1_7/domain/payment/entity/Order.java
@@ -0,0 +1,38 @@
+package com.java.NBE4_5_1_7.domain.payment.entity;
+
+import com.java.NBE4_5_1_7.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, name = "merchant_uid")
+    private String merchantUid; // 주문번호
+
+    @Column(nullable = false, name = "imp_uid")
+    private String impUid; // 결제 고유번호
+
+    @JoinColumn(name = "member_id")
+    @OneToOne(cascade = CascadeType.ALL)
+    private Member member;
+
+    @Column(nullable = false)
+    private BigDecimal amount; // 결제 금액
+
+    @Column(nullable = false)
+    private String status; // 결제 상태 (READY, PAID, CANCELLED 등)
+
+    private LocalDateTime createdAt; // 결제 완료 시간
+}
