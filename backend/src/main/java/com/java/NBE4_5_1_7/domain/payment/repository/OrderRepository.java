9784ab5837d980b3484b0ddea821f56055c1a62d
junho767 <junho767@naver.com>
2025-03-11 14:43:29 +0900

    feat: Order 테이블 생성

---
+++ backend/src/main/java/com/java/NBE4_5_1_7/domain/payment/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.java.NBE4_5_1_7.domain.payment.repository;
+
+import com.java.NBE4_5_1_7.domain.payment.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    Optional<Order> findByMerchantUid(String  merchantUid);
+}
