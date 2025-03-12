2f1e8df3ab5ba39d90573dd021760dbfb9924eab
junho767 <junho767@naver.com>
2025-03-12 12:50:58 +0900

    refacor: 코드 리펙토링 및 트랜잭션 제거

--- backend/src/main/java/com/java/NBE4_5_1_7/domain/payment/repository/OrderRepository.java
+++ backend/src/main/java/com/java/NBE4_5_1_7/domain/payment/repository/OrderRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    Optional<Order> findByMerchantUid(String  merchantUid);
-    Optional<Order> findByImpUid(String  impUid);
+    Optional<Order> findByImpUid(String impUid);
 }
