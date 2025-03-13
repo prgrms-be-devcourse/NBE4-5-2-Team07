a62c81b6eab44ee7997886dc9095e98332247da2
junho767 <junho767@naver.com>
2025-03-13 15:26:02 +0900

    feat: 뉴스 api 기능 구현 - 프론트

--- backend/src/main/java/com/java/NBE4_5_1_7/domain/news/controller/NewsController.java
+++ backend/src/main/java/com/java/NBE4_5_1_7/domain/news/controller/NewsController.java
@@ -3,12 +3,14 @@ package com.java.NBE4_5_1_7.domain.news.controller;
 import com.java.NBE4_5_1_7.domain.news.dto.responseDto.NewResponseDto;
 import com.java.NBE4_5_1_7.domain.news.service.NewsService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/news")
 @RequiredArgsConstructor
@@ -16,7 +18,10 @@ public class NewsController {
     private final NewsService newsService;
 
     @GetMapping
-    public ResponseEntity<NewResponseDto> getNews(@RequestParam String keyWord, int display){
-        return ResponseEntity.ok(newsService.getNaverNews(keyWord,display));
+    public ResponseEntity<NewResponseDto> getNews(
+            @RequestParam String keyWord,
+            @RequestParam(defaultValue = "1") int page
+    ) {
+        return ResponseEntity.ok(newsService.getNaverNews(keyWord, page));
     }
 }
